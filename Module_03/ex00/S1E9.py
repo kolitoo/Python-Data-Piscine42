085b13c8af90582563a273b8e9a2c48c00093a07
Kolitoo <alex2107@outlook.fr>
2024-04-08 15:52:57 +0200

    Classes Modules03

--- Module_03/ex00/S1E9.py
+++ Module_03/ex00/S1E9.py
@@ -1,15 +1,18 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
     """Your docstring for Class"""
     def __init__(self, first_name, is_alive=True):
         """Your docstring for Constructor"""
         self.first_name = first_name
         self.is_alive = is_alive
+
     @abstractmethod
     def die():
         """Your docstring for Method"""
         pass
+
 
 class Stark(Character):
     """Your docstring for Class"""
@@ -17,6 +20,7 @@ class Stark(Character):
         """Your docstring for Constructor"""
         self.first_name = first_name
         self.is_alive = is_alive
+
     def die(self):
         """Your docstring for Method"""
         self.is_alive = False
