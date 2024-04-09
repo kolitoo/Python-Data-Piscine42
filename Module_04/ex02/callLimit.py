de6b7ff07b61ace5c46bea9f23b1a9ad10cd6021
Kolitoo <alex2107@outlook.fr>
2024-04-09 11:49:20 +0200

    ex02-Modules04

--- Module_04/ex02/callLimit.py
+++ Module_04/ex02/callLimit.py
@@ -1,6 +1,20 @@
 from typing import Any
 
+
 def callLimit(limit: int):
-    count = 0
+    """
+    This function blocks his execution above a limite.
+    When we call @callLimit(n), it will call callLimiter()
+    with our function and it will call limit_function to 
+    check if we can execute is or not."""
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
+        def limit_function(*args: Any, **kwargs: Any):
+            if limit_function.counter < limit:
+                limit_function.counter += 1
+                return function(*args, **kwargs)
+            else:
+                print(f"Error: {function} call too many times")
+                return None
+        limit_function.counter = 0
+        return limit_function
+    return callLimiter
